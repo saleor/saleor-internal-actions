c13ae585af5a558b4be06c61314396bfc81fe68a
GrzegorzKowalik <kowalik.grzegorz@outlook.com>
2020-11-10 08:37:58 +0100

    Remove hack from multitenant middleware (#75)

--- tenants/middleware.py
+++ tenants/middleware.py
@@ -1,22 +1,11 @@
-from tenant_schemas.middleware import BaseTenantMiddleware
+from tenant_schemas.middleware import TenantMiddleware
 
 
-class SaleorTenantMiddleware(BaseTenantMiddleware):
-    EXCLUDED_PATHS = [
-        "/prometheus/metrics"
-    ]
+class SaleorTenantMiddleware(TenantMiddleware):
+
+    EXCLUDED_PATHS = ["/prometheus/metrics"]
 
     def process_request(self, request):
         if request.path in SaleorTenantMiddleware.EXCLUDED_PATHS:
             return
         return super().process_request(request)
-
-    def get_tenant(self, model, hostname, request):
-        return model.objects.get(domain_url=self.get_base_domain_url(hostname))
-
-    def get_base_domain_url(self, hostname):
-        domain_parts = hostname.split(".")
-        tenant_label_parts = domain_parts[0].split("-")
-        if len(tenant_label_parts) > 1:
-            domain_parts[0] = tenant_label_parts[-1]
-        return ".".join(domain_parts)
