5d6d678e81825ad154a9dffa6cb6318d66cdd939
Mikail Kocak <mikail.kocak@saleor.io>
2024-09-16 15:26:05 +0200

    Add SBOM generator action
    This adds a GitHub Action that generates a Software Bill of Material (SBOM) using the [CycloneDX] format (via the [`cdxgen`] command).
    
    This allows us to generate a list of dependencies (main & transient), and fetch the licenses (from PyPI and NPM for example).
    
    [`cdxgen`]: https://github.com/CycloneDX/cdxgen/
    [CycloneDX]: https://cyclonedx.org/

---
+++ sbom-generator/scripts/generate-sbom.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+set -eu -o pipefail
+
+function log() {
+    msg="$1"
+    shift
+    # shellcheck disable=SC2059 # "$1" for printf is assumed safe.
+    printf "$msg\n" "$@" >&2
+}
+
+# Add extra logging if the runner was run with debug logging.
+test -z "${RUNNER_DEBUG+x}" || set -x
+
+# User provided preferences:
+# - CONF_PROJECT_DIR: the path of the project to scan (relative or absolute).
+# - CONF_ECOSYSTEMS: list of ecosystems to scan (e.g., python), whitespace separated.
+# - CONF_RESULT_PATH: the path where to store the SBOM.
+CONF_PROJECT_DIR="${CONF_PROJECT_DIR:-$PWD}"
+CONF_ECOSYSTEMS="${CONF_ECOSYSTEMS:-}"
+CONF_RESULT_PATH=${CONF_RESULT_PATH:-./bom.json}
+
+cmd_args=(
+    "--recurse"
+    "--output=$CONF_RESULT_PATH"
+    "--profile=license-compliance"
+    # Path to the source code to analyze (inside the mounted container)
+    "$CONF_PROJECT_DIR"
+)
+
+# Add project types into the 'cdxgen' command line argument list.
+read -d '' -r -a ecosystems < <(echo "$CONF_ECOSYSTEMS") || true
+for ecosystem in "${ecosystems[@]}"; do
+    cmd_args+=( "--type=$ecosystem" )
+done
+
+# Generate the BOM.
+log "Generating SBOM..."
+FETCH_LICENSE=true cdxgen "${cmd_args[@]}"
